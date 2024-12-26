05f85724dc1303e572856a56a9a50c74b61912a2
Michael Liebold <39069426+mliebold@users.noreply.github.com>
2024-12-26 18:48:26 -0500

    add gallery spacing and zoom effect on hover

--- src/components/landing/gallery.tsx
+++ src/components/landing/gallery.tsx
@@ -16,35 +16,48 @@ export default async function Gallery() {
       // Add the landscape image
       <div
         key={`landscape-${index}`}
-        className="col-span-2 h-[30rem] relative hover:shadow-lg transition-shadow duration-300"
+        className="col-span-2 h-[30rem] relative overflow-hidden"
       >
-        <Image
-          src={landscape.url}
-          fill
-          alt="Landscape image"
-          className="object-cover"
-        />
+        <div className="h-full w-full relative hover:shadow-lg transition-transform duration-300 hover:scale-105">
+          <Image
+            src={landscape.url}
+            fill
+            alt="Landscape image"
+            className="object-cover"
+          />
+        </div>
+        ,
       </div>,
     ];
 
     // Add the pair of portrait images if available
     if (portraitPair.length === 2) {
       elements.push(
-        <div key={`portrait-left-${index}`} className="h-[40rem] relative">
-          <Image
-            src={portraitPair[0].url}
-            fill
-            alt="Portrait image"
-            className="object-cover"
-          />
+        <div
+          key={`portrait-left-${index}`}
+          className="h-[40rem] relative overflow-hidden"
+        >
+          <div className="h-full w-full relative hover:shadow-lg transition-transform duration-300 hover:scale-105">
+            <Image
+              src={portraitPair[0].url}
+              fill
+              alt="Portrait image"
+              className="object-cover"
+            />
+          </div>
         </div>,
-        <div key={`portrait-right-${index}`} className="h-[40rem] relative">
-          <Image
-            src={portraitPair[1].url}
-            fill
-            alt="Portrait image"
-            className="object-cover"
-          />
+        <div
+          key={`portrait-right-${index}`}
+          className="h-[40rem] relative overflow-hidden"
+        >
+          <div className="h-full w-full relative hover:shadow-lg transition-transform duration-300 hover:scale-105">
+            <Image
+              src={portraitPair[1].url}
+              fill
+              alt="Portrait image"
+              className="object-cover"
+            />
+          </div>
         </div>
       );
     }
@@ -52,5 +65,5 @@ export default async function Gallery() {
     return elements;
   });
 
-  return <div className="grid grid-cols-2">{galleryElements}</div>;
+  return <div className="grid grid-cols-2 gap-2 px-2">{galleryElements}</div>;
 }
