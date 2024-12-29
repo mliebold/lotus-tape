e74fb986ea1c9d1d9be090e895b893b6ef22c684
Michael Liebold <39069426+mliebold@users.noreply.github.com>
2024-12-28 22:42:52 -0500

    remove unused import to fix build errors

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "@/app/styles/globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
-import Navbar from "@/components/layout/navbar";
 import Footer from "@/components/layout/footer";
 
 export const metadata: Metadata = {
