090eb365ae29bf68f8356311525c5d11b7eef793
Michael Liebold <39069426+mliebold@users.noreply.github.com>
2024-12-30 10:57:38 -0500

    add randomfilenamesuffix utility

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { randomUUID } from "crypto";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function addRandomFileNameSuffix(fileName: string) {
+  const fileNameParts = fileName.split(".");
+  const randomSuffix = randomUUID();
+  return `${fileNameParts[0]}-${randomSuffix}.${fileNameParts[1]}`;
 }
