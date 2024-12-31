23e11869d57f177c102c2ca7c8b68a0578b9dafe
Michael Liebold <39069426+mliebold@users.noreply.github.com>
2024-12-31 16:17:21 -0500

    Add html sanitization

--- src/app/(frontend)/(general)/music/page.tsx
+++ src/app/(frontend)/(general)/music/page.tsx
@@ -1,31 +1,45 @@
+import DOMPurify from "dompurify";
+import { JSDOM } from "jsdom";
+
 export default function MusicPage() {
-  const trackUuids = [
-    "7hiRSxfVaYUWhIddOtw6Y1",
-    "65Plu14QWS6vOmq6aUaCIw",
-    "3TOkJ9RIUJIwcAybyCZdri",
-    "0XgwqNPqPpS51Lbr4g998B",
-    "5EMoEdaEJlwiHiBSWGBoO1",
-    "2wVw68d7C5ZCsWT01LMArE",
+  const iframes = [
+    '<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1769456373&color=%2314130a&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/lotustape" title="Lotus Tape" target="_blank" style="color: #cccccc; text-decoration: none;">Lotus Tape</a> · <a href="https://soundcloud.com/lotustape/lotus-tape-knock-1" title="Lotus Tape - KNOCK" target="_blank" style="color: #cccccc; text-decoration: none;">Lotus Tape - KNOCK</a></div>',
+    '<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1615739508&color=%2314130a&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/lotustape" title="Lotus Tape" target="_blank" style="color: #cccccc; text-decoration: none;">Lotus Tape</a> · <a href="https://soundcloud.com/lotustape/lotus-tape-og-freestyle-3" title="Lotus Tape - OG FREESTYLE" target="_blank" style="color: #cccccc; text-decoration: none;">Lotus Tape - OG FREESTYLE</a></div>',
   ];
+
+  const window = new JSDOM("").window;
+  const DOMPurifyServer = DOMPurify(window);
 
   return (
     <section className="grid py-16 mt-[4.5rem] mx-5">
       <h1 className="text-5xl font-extrabold">Music</h1>
       <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 mt-10">
-        {trackUuids.map((trackUuid) => (
-          <iframe
-            key={trackUuid}
-            style={{
-              borderRadius: "20px",
-            }}
-            src={`https://open.spotify.com/embed/track/${trackUuid}?utm_source=generator&theme=0`}
-            width="100%"
-            height="152"
-            frameBorder="0"
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-            loading="eager"
-          ></iframe>
-        ))}
+        {iframes.map((iframe) => {
+          const cleanHtml = DOMPurifyServer.sanitize(iframe, {
+            ALLOWED_TAGS: ["iframe", "div", "a"],
+            ALLOWED_ATTR: [
+              "width",
+              "height",
+              "scrolling",
+              "frameborder",
+              "allow",
+              "src",
+              "style",
+              "href",
+              "title",
+              "target",
+            ],
+          });
+
+          return (
+            <div
+              className=""
+              dangerouslySetInnerHTML={{
+                __html: cleanHtml,
+              }}
+            ></div>
+          );
+        })}
       </div>
     </section>
   );
