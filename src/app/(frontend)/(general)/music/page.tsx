25a95dd411c774ea9c25f30707f53df42cd54079
Michael Liebold <mliebold95@outlook.com>
2025-01-01 13:40:48 -0500

    Make Music Page CMS Based (#14)
    * Add html sanitization
    
    * Add music collection for payload
    
    * make music page based on new music collection

--- src/app/(frontend)/(general)/music/page.tsx
+++ src/app/(frontend)/(general)/music/page.tsx
@@ -1,31 +1,51 @@
-export default function MusicPage() {
-  const trackUuids = [
-    "7hiRSxfVaYUWhIddOtw6Y1",
-    "65Plu14QWS6vOmq6aUaCIw",
-    "3TOkJ9RIUJIwcAybyCZdri",
-    "0XgwqNPqPpS51Lbr4g998B",
-    "5EMoEdaEJlwiHiBSWGBoO1",
-    "2wVw68d7C5ZCsWT01LMArE",
-  ];
+import DOMPurify from "dompurify";
+import { JSDOM } from "jsdom";
+import { getPayload } from "payload";
+import configPromise from "@payload-config";
+
+const payload = await getPayload({ config: configPromise });
+
+export default async function MusicPage() {
+  const music = await payload.find({
+    collection: "music",
+    sort: "-releaseDate",
+  });
+
+  const window = new JSDOM("").window;
+  const DOMPurifyServer = DOMPurify(window);
 
   return (
     <section className="grid py-16 mt-[4.5rem] mx-5">
       <h1 className="text-5xl font-extrabold">Music</h1>
       <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 mt-10">
-        {trackUuids.map((trackUuid) => (
-          <iframe
-            key={trackUuid}
-            style={{
-              borderRadius: "20px",
-            }}
-            src={`https://open.spotify.com/embed/track/${trackUuid}?utm_source=generator&theme=0`}
-            width="100%"
-            height="152"
-            frameBorder="0"
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-            loading="eager"
-          ></iframe>
-        ))}
+        {music.docs.map((track, index) => {
+          if (track.trackEmbed) {
+            const cleanHtml = DOMPurifyServer.sanitize(track.trackEmbed, {
+              ALLOWED_TAGS: ["iframe", "div", "a"],
+              ALLOWED_ATTR: [
+                "width",
+                "height",
+                "scrolling",
+                "frameborder",
+                "allow",
+                "src",
+                "style",
+                "href",
+                "title",
+                "target",
+              ],
+            });
+
+            return (
+              <div
+                key={index}
+                dangerouslySetInnerHTML={{
+                  __html: cleanHtml,
+                }}
+              ></div>
+            );
+          }
+        })}
       </div>
     </section>
   );
